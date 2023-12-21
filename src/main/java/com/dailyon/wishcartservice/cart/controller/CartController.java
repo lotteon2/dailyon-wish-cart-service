8057e685684cbad64d41f360b330eb57cc114d8c
wakkpu <ho0524870@gmail.com>
2023-12-21 21:42:57 +0900

    O2F-894 [FEAT] 장바구니 조회 controller

--- src/main/java/com/dailyon/wishcartservice/cart/controller/CartController.java
+++ src/main/java/com/dailyon/wishcartservice/cart/controller/CartController.java
@@ -2,31 +2,35 @@ package com.dailyon.wishcartservice.cart.controller;
 
 import com.dailyon.wishcartservice.cart.dto.request.DeleteCartListRequest;
 import com.dailyon.wishcartservice.cart.dto.request.UpsertCartRequest;
-import com.dailyon.wishcartservice.cart.service.CartService;
+import com.dailyon.wishcartservice.cart.facade.CartFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RequestMapping("/cart")
 @RestController
 @RequiredArgsConstructor
 public class CartController {
-    private final CartService cartService;
+    private final CartFacade cartFacade;
 
     @PostMapping
     public ResponseEntity<Void> upsertCart(@RequestHeader(name = "memberId") Long memberId,
                                            @Valid @RequestBody UpsertCartRequest upsertCartRequest) {
-        cartService.upsert(memberId, upsertCartRequest);
+        cartFacade.upsert(memberId, upsertCartRequest);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping
     public ResponseEntity<Void> deleteCart(@RequestHeader(name = "memberId") Long memberId,
                                            @Valid @RequestBody DeleteCartListRequest deleteCartListRequest) {
-        cartService.deleteCarts(memberId, deleteCartListRequest);
+        cartFacade.delete(memberId, deleteCartListRequest);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<?> readCart(@RequestHeader(name = "memberId") Long memberId) {
+        return ResponseEntity.ok(cartFacade).build();
     }
 }
