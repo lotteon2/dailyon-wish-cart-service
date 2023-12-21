74f1bb93734e708a19456b77b20b7f07da3a129f
wakkpu <ho0524870@gmail.com>
2023-12-21 21:41:03 +0900

    [FIX] feign 관련 설정

--- src/main/java/com/dailyon/wishcartservice/WishCartServiceApplication.java
+++ src/main/java/com/dailyon/wishcartservice/WishCartServiceApplication.java
@@ -3,9 +3,11 @@ package com.dailyon.wishcartservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableFeignClients
 @EnableMongoAuditing
 @EnableMongoRepositories
 @EnableDiscoveryClient
