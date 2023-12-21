955323fa5dfde8b53ece8c469a89256b9b601f1a
wakkpu <ho0524870@gmail.com>
2023-12-21 11:52:53 +0900

    O2F-769 [FEAT] 장바구니 Document

--- src/main/java/com/dailyon/wishcartservice/cart/entity/Cart.java
+++ src/main/java/com/dailyon/wishcartservice/cart/entity/Cart.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -24,6 +26,13 @@ public class Cart {
     @Field(targetType = FieldType.ARRAY, write = Field.Write.NON_NULL)
     private List<CartItem> cartItems;
 
+    public static Cart init(Long memberId, Long productId, Long productSizeId, Integer quantity, String lastMemberCode) {
+        return Cart.builder()
+                .memberId(memberId)
+                .cartItems(List.of(Cart.CartItem.init(productId, productSizeId, quantity, lastMemberCode)))
+                .build();
+    }
+
     @Getter
     @Builder
     @NoArgsConstructor
@@ -35,10 +44,37 @@ public class Cart {
         @Field(targetType = FieldType.INT64, write = Field.Write.NON_NULL)
         private Long productSizeId;
 
-        @Field(targetType = FieldType.INT64, write = Field.Write.NON_NULL)
-        private Long quantity;
+        @Field(targetType = FieldType.INT32, write = Field.Write.NON_NULL)
+        private Integer quantity;
 
         @Field(targetType = FieldType.STRING, write = Field.Write.ALWAYS)
         private String lastMemberCode;
+
+        @CreatedDate
+        @Builder.Default
+        @Field(targetType = FieldType.DATE_TIME, write = Field.Write.NON_NULL)
+        private LocalDateTime createdAt = LocalDateTime.now();
+
+        @LastModifiedDate
+        @Builder.Default
+        @Field(targetType = FieldType.DATE_TIME, write = Field.Write.NON_NULL)
+        private LocalDateTime updatedAt = LocalDateTime.now();
+
+        public static Cart.CartItem init(Long productId, Long productSizeId, Integer quantity, String lastMemberCode) {
+            return Cart.CartItem.builder()
+                    .productId(productId)
+                    .productSizeId(productSizeId)
+                    .quantity(quantity)
+                    .lastMemberCode(lastMemberCode)
+                    .build();
+        }
+
+        public void setQuantity(Integer quantity) {
+            this.quantity = quantity;
+        }
+
+        public void setLastMemberCode(String lastMemberCode) {
+            this.lastMemberCode = lastMemberCode;
+        }
     }
 }
