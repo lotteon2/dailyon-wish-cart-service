c2136bd4b8ed7501e9f4054cf645ad223451c435
wakkpu <ho0524870@gmail.com>
2023-12-22 10:48:46 +0900

    O2F-894 [FEAT] feign response

--- src/main/java/com/dailyon/wishcartservice/cart/controller/CartController.java
+++ src/main/java/com/dailyon/wishcartservice/cart/controller/CartController.java
@@ -2,8 +2,12 @@ package com.dailyon.wishcartservice.cart.controller;
 
 import com.dailyon.wishcartservice.cart.dto.request.DeleteCartListRequest;
 import com.dailyon.wishcartservice.cart.dto.request.UpsertCartRequest;
+import com.dailyon.wishcartservice.cart.dto.response.ReadCartPageResponse;
 import com.dailyon.wishcartservice.cart.facade.CartFacade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,7 +34,8 @@ public class CartController {
     }
 
     @GetMapping
-    public ResponseEntity<?> readCart(@RequestHeader(name = "memberId") Long memberId) {
-        return ResponseEntity.ok(cartFacade).build();
+    public ResponseEntity<ReadCartPageResponse> readCart(@RequestHeader(name = "memberId") Long memberId,
+                                                         @PageableDefault(sort = "updatedAt", direction = Sort.Direction.DESC) Pageable pageable) {
+        return ResponseEntity.ok(cartFacade.read(memberId, pageable));
     }
 }
