9fe88be0f5676b908463a9e33a9c251cc0ce784b
wakkpu <ho0524870@gmail.com>
2023-12-22 10:50:34 +0900

    [FEAT] facade

--- src/main/java/com/dailyon/wishcartservice/cart/facade/CartFacade.java
+++ src/main/java/com/dailyon/wishcartservice/cart/facade/CartFacade.java
@@ -2,18 +2,20 @@ package com.dailyon.wishcartservice.cart.facade;
 
 import com.dailyon.wishcartservice.cart.dto.request.DeleteCartListRequest;
 import com.dailyon.wishcartservice.cart.dto.request.UpsertCartRequest;
-import com.dailyon.wishcartservice.cart.dto.response.ReadCartListResponse;
+import com.dailyon.wishcartservice.cart.dto.response.ReadCartPageResponse;
 import com.dailyon.wishcartservice.cart.entity.Cart;
 import static com.dailyon.wishcartservice.cart.entity.Cart.CartItem;
 import com.dailyon.wishcartservice.cart.service.CartService;
 import com.dailyon.wishcartservice.common.feign.client.ProductFeignClient;
 import com.dailyon.wishcartservice.common.feign.request.ReadWishCartProductRequest;
-import com.dailyon.wishcartservice.common.feign.response.ReadWishCartProductListResponse;
-import static com.dailyon.wishcartservice.common.feign.response.ReadWishCartProductListResponse.ReadWishCartProductResponse;
+import com.dailyon.wishcartservice.common.feign.response.ReadWishCartProductMapResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
@@ -30,13 +32,15 @@ public class CartFacade {
         cartService.deleteCarts(memberId, deleteCartListRequest);
     }
 
-    public ReadCartListResponse read(Long memberId) {
-        return cartService.read(memberId).map(
-                cart -> ReadCartListResponse.create(
-                        cart, productFeignClient.readWishCartProducts(cart.getCartItems().stream()
-                                .map(ReadWishCartProductRequest::fromEntity)
-                                .collect(Collectors.toList())).getBody()
-                )
-        ).orElseGet(() -> ReadCartListResponse.builder().responses(new ArrayList<>()).build());
+    public ReadCartPageResponse read(Long memberId, Pageable pageable) {
+        Page<CartItem> cartItems = cartService.readPages(memberId, pageable);
+
+        List<ReadWishCartProductRequest> requests = cartItems.stream()
+                .map(ReadWishCartProductRequest::fromEntity)
+                .collect(Collectors.toList());
+
+        ReadWishCartProductMapResponse response = productFeignClient.readWishCartProducts(requests).getBody();
+
+        return ReadCartPageResponse.create(cartItems, response);
     }
 }
