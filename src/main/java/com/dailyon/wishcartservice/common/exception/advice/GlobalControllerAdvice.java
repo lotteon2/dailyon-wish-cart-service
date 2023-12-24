1fc7270c9d1022f3a8053012260904109d851a7c
전종민 <ho05248@naver.com>
2023-12-25 02:48:33 +0900

    [FEAT] 예외 처리

--- src/main/java/com/dailyon/wishcartservice/common/exception/advice/GlobalControllerAdvice.java
+++ src/main/java/com/dailyon/wishcartservice/common/exception/advice/GlobalControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.dailyon.wishcartservice.common.exception.advice;
 
 import com.dailyon.wishcartservice.common.exception.ErrorResponse;
+import com.dailyon.wishcartservice.common.exception.NotExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,6 +17,14 @@ public class GlobalControllerAdvice {
     public ErrorResponse validException(org.springframework.validation.BindException e) {
         return ErrorResponse.builder()
                 .message(Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage())
+                .build();
+    }
+
+    @ExceptionHandler(NotExistsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse notExistsException(NotExistsException e) {
+        return ErrorResponse.builder()
+                .message(e.getMessage())
                 .build();
     }
 }
