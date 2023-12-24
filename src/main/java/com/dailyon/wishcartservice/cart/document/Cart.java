f8a898e6d059f94d5e806cfc0687b88bec55ddb2
전종민 <ho05248@naver.com>
2023-12-25 02:00:21 +0900

    [FIX] cart document 변경 - sub document 형태로 사용 안하기로 했음 - index도 걸 수 없고, CRUD시 로직 모두 복잡함

--- src/main/java/com/dailyon/wishcartservice/cart/document/Cart.java
+++ src/main/java/com/dailyon/wishcartservice/cart/document/Cart.java
@@ -6,93 +6,87 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Document
+@CompoundIndexes({
+        @CompoundIndex(
+                name = "cart_unique_idx",
+                def = "{'memberId' : 1, 'productId' : 1, 'productSizeId' : 1}",
+                unique = true
+        )
+})
 public class Cart {
-    @MongoId(targetType = FieldType.INT64)
+    @MongoId(targetType = FieldType.OBJECT_ID)
+    private String id;
+
+    @Indexed(name = "cart_member_idx", direction = IndexDirection.ASCENDING)
+    @Field(name = "memberId", targetType = FieldType.INT64, write = Field.Write.NON_NULL)
     private Long memberId;
 
-    @Field(name = "cartItems", targetType = FieldType.ARRAY, write = Field.Write.NON_NULL)
-    private List<CartItem> cartItems;
+    @Field(name = "productId", targetType = FieldType.INT64, write = Field.Write.NON_NULL)
+    private Long productId;
 
-    public static Cart init(Long memberId, Long productId, Long productSizeId, Long quantity, String lastMemberCode) {
+    @Field(name = "productSizeId", targetType = FieldType.INT64, write = Field.Write.NON_NULL)
+    private Long productSizeId;
+
+    @Field(name = "quantity", targetType = FieldType.INT64, write = Field.Write.NON_NULL)
+    private Long quantity;
+
+    @Field(name = "lastMemberCode", targetType = FieldType.STRING, write = Field.Write.ALWAYS)
+    private String lastMemberCode;
+
+    @CreatedDate
+    @Builder.Default
+    @Field(name = "createdAt", targetType = FieldType.DATE_TIME, write = Field.Write.NON_NULL)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @LastModifiedDate
+    @Builder.Default
+    @Field(name = "updateAt", targetType = FieldType.DATE_TIME, write = Field.Write.NON_NULL)
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    public static Cart create(Long memberId, Long productId, Long productSizeId, Long quantity, String lastMemberCode) {
         return Cart.builder()
                 .memberId(memberId)
-                .cartItems(List.of(Cart.CartItem.create(productId, productSizeId, quantity, lastMemberCode)))
+                .productId(productId)
+                .productSizeId(productSizeId)
+                .quantity(quantity)
+                .lastMemberCode(lastMemberCode)
                 .build();
     }
 
-    public static Cart init(Long memberId, Long productId, Long productSizeId, Long quantity) {
+    public static Cart create(Long memberId, Long productId, Long productSizeId, Long quantity) {
         return Cart.builder()
                 .memberId(memberId)
-                .cartItems(List.of(Cart.CartItem.create(productId, productSizeId, quantity)))
+                .productId(productId)
+                .productSizeId(productSizeId)
+                .quantity(quantity)
                 .build();
     }
 
-    @Getter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class CartItem {
-        @Field(name = "productId", targetType = FieldType.INT64, write = Field.Write.NON_NULL)
-        private Long productId;
+    public String toKey() {
+        return "pid=" + this.productId + "&sid=" + this.productSizeId;
+    }
 
-        @Field(name = "productSizeId", targetType = FieldType.INT64, write = Field.Write.NON_NULL)
-        private Long productSizeId;
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
 
-        @Field(name = "quantity", targetType = FieldType.INT64, write = Field.Write.NON_NULL)
-        private Long quantity;
-
-        @Field(name = "lastMemberCode", targetType = FieldType.STRING, write = Field.Write.ALWAYS)
-        private String lastMemberCode;
-
-        @CreatedDate
-        @Builder.Default
-        @Field(name = "createdAt", targetType = FieldType.DATE_TIME, write = Field.Write.NON_NULL)
-        private LocalDateTime createdAt = LocalDateTime.now();
-
-        @LastModifiedDate
-        @Builder.Default
-        @Field(name = "updateAt", targetType = FieldType.DATE_TIME, write = Field.Write.NON_NULL)
-        private LocalDateTime updatedAt = LocalDateTime.now();
-
-        public static Cart.CartItem create(Long productId, Long productSizeId, Long quantity, String lastMemberCode) {
-            return CartItem.builder()
-                    .productId(productId)
-                    .productSizeId(productSizeId)
-                    .quantity(quantity)
-                    .lastMemberCode(lastMemberCode)
-                    .build();
-        }
-        public static Cart.CartItem create(Long productId, Long productSizeId, Long quantity) {
-            return CartItem.builder()
-                    .productId(productId)
-                    .productSizeId(productSizeId)
-                    .quantity(quantity)
-                    .build();
-        }
-
-        public String toKey() {
-            return "pid=" + this.productId + "&sid=" + this.productSizeId;
-        }
-
-        public void setQuantity(Long quantity) {
-            this.quantity = quantity;
-        }
-
-        public void setLastMemberCode(String lastMemberCode) {
-            this.lastMemberCode = lastMemberCode;
-        }
+    public void setLastMemberCode(String lastMemberCode) {
+        this.lastMemberCode = lastMemberCode;
     }
 }
