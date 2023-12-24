bd50ce35fe3bf8444455770c9254ac13cd135b57
전종민 <ho05248@naver.com>
2023-12-24 23:28:32 +0900

    O2F-902 [FEAT] 찜 목록 조회 controller

---
+++ src/test/java/com/dailyon/wishcartservice/controller/WishListControllerTests.java
@@ -0,0 +1,57 @@
+package com.dailyon.wishcartservice.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class WishListControllerTests {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("자신의 찜 목록 조회 성공")
+    void readWishListSuccess1() throws Exception {
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                get("/wish-list")
+                        .header("memberId", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @DisplayName("남의 찜 목록 조회 성공")
+    void readWishListSuccess2() throws Exception {
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                get("/wish-list")
+                        .header("memberId", 1L)
+                        .header("targetId", 2L)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
